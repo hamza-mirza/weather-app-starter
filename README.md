7d37e3b560150fb952b65dbe98ed89f02dac783b
Hamza Shahid <hammzzeeee@gmail.com>
2020-05-03 16:55:06 +0100

    starter files ready

---
+++ README.md
@@ -0,0 +1,5 @@
+# Starter files for my YouTube tutorial - React Weather App with Hooks and Context | React.js tutroial 2020
+
+* Download or clone the repository
+* Open project in the terminal and install the node modules - npm install
+* Run npm start to launch the project.
---
+++ templates/Weather.js
@@ -0,0 +1,17 @@
+<div className="weather-data">
+  <p className="weather__tagline">Weather forecast for <span className="weather-data__city">Manchester</span></p>
+  <div className="weather-data__box">
+    <span className="weather-data__property">
+      <p className="weather-data__title">Temperature</p>
+      <p className="weather-data__value">10.9</p>
+    </span>
+    <span className="weather-data__property">
+      <p className="weather-data__title">Humidity</p>
+      <p className="weather-data__value">22</p>
+    </span>
+    <span className="weather-data__property">
+      <p className="weather-data__title">Pressure</p>
+      <p className="weather-data__value">1001</p>
+    </span>
+  </div>
+</div>
